b90322b0087f5093aab47a72031be845b79bf5d6
Aditya Pradhan <contact.adityapradhan.me@gmail.com>
2026-01-04 13:34:24 +0530

    WIP

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { metadata, module } from 'src/common/metadata';
+import { SupabaseService } from './supabase.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly supabaseService: SupabaseService) {}
+
   signup() {
     throw new ForbiddenException({
       code: metadata.AUTH.INVALID,
--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Global, Module } from '@nestjs/common';
-import { UsersRepository } from './users.repository';
+import { UsersService } from './users.service';
 
 @Global()
 @Module({
-  providers: [UsersRepository],
-  exports: [UsersRepository],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
